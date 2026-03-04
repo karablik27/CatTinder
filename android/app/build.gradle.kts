558e30d35af2e2f795c42c99c1c1d8e70c118c88
karabelnikov.s <karabelnikov.s@rwb.ru>
2026-03-04 23:06:30 +0300

    ci

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.cattinder"
+    namespace = "com.example.cattinder1"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.cattinder"
+        applicationId = "com.example.cattinder1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
